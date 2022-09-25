feb7ce60ad9b29f9122e942642cd65aa7e8e2371
alfa <muhamadalfarizy20@gmail.com>
2022-09-26 03:51:36 +0700

    menambahkan macammacam pemanggilan dan contoh destructing

--- destructing.js
+++ destructing.js
@@ -1,8 +1,24 @@
-let resepDonat = {
+const bahan = {
     kering: {
         bahan1: 'terigu',
         bahan2: 'ragi',
-        bahan3: 'gula'
+        bahan3: 'gula',
     },
-    basah: 'susu'
+    basah: 'susu',
+    lainnya: 'butter'
+    
+};
+// pemanggilan biasa menggunakan parameter
+// function resepDonat(kering, basah, lainnya){
+//     console.log(kering, basah, lainnya);
+// }
+// resepDonat(bahan.kering.bahan1, bahan.basah, bahan.lainnya);
+
+// pemanggilan menggunakan variable object - 
+function resepDonat2({kering: { bahan1, bahan2, bahan3}, basah, lainnya}) {
+    return ` `
 }
+console.log(resepDonat2(bahan));
+
+
+ 
