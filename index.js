94d10f20e6cf516ce444dfdaf6f586eecd3110ab
alfa <muhamadalfarizy20@gmail.com>
2022-09-25 13:15:48 +0700

    membuat operator perbandingan nilai

---
+++ index.js
@@ -0,0 +1,15 @@
+// membuat operator logika
+
+let nilai =prompt('MAsukan Nilai Mu :')
+
+if ( nilai >= 90){
+    prompt('Nilaimu A');
+} else if ( nilai >= 80){
+    prompt('Nilai Mu B');
+} else if ( nilai >= 70){
+    prompt('Nilai Mu B');
+}  else if ( nilai >= 60){
+    prompt('Nilai Mu B');
+} else {
+    alert('Nilai mu E Cobalagi!')
+}
